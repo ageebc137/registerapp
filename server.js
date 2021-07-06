b29ed2b91dcc03e6da62382eb4e1c2c38de6a1ee
Allister Bernal <ageebernal@allisters-mbp.attlocal.net>
2021-07-05 23:27:22 -0700

    fix config

--- server.js
+++ server.js
@@ -11,11 +11,12 @@ const PORT = process.env.PORT || 5000;
 
 db.connectToDB();
 
+app.use(express.static(path.join(__dirname, './client/build')));
 app.use(express.json());
 app.use(cors())
 
 app.use("/db", userRouter);
-app.use(express.static(path.join(__dirname, './client/build')));
+
 app.get('*', (req,res) => res.sendFile(path.resolve(__dirname), './client/build/index.html'));
 
 app.listen(PORT, () => console.log("You are now listening on", PORT));
