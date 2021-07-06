762c123217b92ed3a8e26069e5e3e0a287df9b57
Allister Bernal <ageebernal@allisters-mbp.attlocal.net>
2021-07-06 08:40:53 -0700

    add photo

--- client/src/components/subcomponent/Header.js
+++ client/src/components/subcomponent/Header.js
@@ -2,11 +2,13 @@ import { Button } from 'react-bootstrap';
 import { useHistory } from 'react-router';
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
+import logo from '../../assets/users-logo.png';
 
 export default function Header(props) {
     return(
         <header>
             <div id="title">
+                    <img src={logo} />
                     <h1>Registration Database</h1>
                     <Button onClick={props.buttonFunction} >{props.buttonName}</Button>
             </div>  
